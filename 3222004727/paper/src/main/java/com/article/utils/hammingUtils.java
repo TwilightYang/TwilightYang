7da15c3ea9972f5b0ce951c4d0735b98718628e7
Twilight <3512133769@qq.com>
2024-03-12 21:19:02 +0800

    main

---
+++ 3222004727/paper/src/main/java/com/article/utils/hammingUtils.java
@@ -0,0 +1,39 @@
+package com.article.utils;
+
+public class hammingUtils {
+    /**
+     * 计算两个SimHash值的海明距离
+     *
+     * @param simHash1 第一个SimHash值
+     * @param simHash2 第二个SimHash值
+     * @return 海明距离
+     */
+    public static int calculateHammingDistance(String simHash1, String simHash2) {
+        // 检查两个SimHash值的长度是否相同
+        if (simHash1.length() != simHash2.length()) {
+            throw new IllegalArgumentException("错误：SimHash值长度不相同！！");
+        }
+
+        int distance = 0;
+        // 遍历两个SimHash值的每一位，比较是否相同
+        for (int i = 0; i < simHash1.length(); i++) {
+            if (simHash1.charAt(i) != simHash2.charAt(i)) {
+                distance++;
+            }
+        }
+        return distance;
+    }
+
+    /**
+     * 输入两个simHash值，输出相似度
+     *
+     * @param simHash1 第一个SimHash值
+     * @param simHash2 第二个SimHash值
+     * @return 相似度
+     */
+    public static double calculateSimilarity(String simHash1, String simHash2) {
+        int hammingDistance = calculateHammingDistance(simHash1, simHash2);
+        int totalBits = simHash1.length();
+        return  (1 - (double)hammingDistance / totalBits) * 100;
+    }
+}
---
+++ 3222004727/paper/src/main/java/com/article/utils/txtUtils.java
@@ -0,0 +1,42 @@
+package com.article.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class txtUtils {
+    /**
+     * 读取文件
+     * @param filePath 目标文件路径
+     * @return 文件内容
+     */
+    public static String readFile(String filePath) {
+        StringBuilder str = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null){
+                str.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str.toString();
+    }
+
+    /**
+     * 写入文件
+     * @param fileElem 写入的内容
+     * @param filePath 写入的文件路径
+     */
+    public static void writeFile(double fileElem, String filePath) {
+        String str = Double.toString(fileElem);
+        try(FileWriter fw = new FileWriter(filePath, true)){
+            fw.write(str,0,(str.length() > 3 ? 4 : str.length()));
+            fw.write("\r\n");
+            fw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
