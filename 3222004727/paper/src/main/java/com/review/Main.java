dadb29431046aa1b9c8379d60b0f8cd5fe255b70
Twilight <3512133769@qq.com>
2024-03-14 09:46:59 +0800

    修改

--- 3222004727/paper/src/main/java/com/review/Main.java
+++ 3222004727/paper/src/main/java/com/review/Main.java
@@ -13,19 +13,12 @@ public class Main {
             System.out.println("请提供正确的参数：[原文文件] [抄袭版论文的文件] [答案文件]");
             return;
         }
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.print("请输入原文文件地址：");
-//        String path0 = scanner.nextLine();
-//
-//        System.out.print("请输入对比文件地址：");
-//        String path1 = scanner.nextLine();
-//
-//        System.out.print("请输入结果文件地址：");
-//        String answerFile = scanner.nextLine();
 
         // 从命令行输入的路径名读取对应的文件
         // 文件内容转化为字符串
+//        String str0 = TxtUtils.readFile("D:/test/orig.txt");
+//        String str1 = TxtUtils.readFile("D:/test/orig_0.8_add.txt");
+//        String answerFile = "D:/test/answer.txt";
         String str0 = TxtUtils.readFile(args[0]);
         String str1 = TxtUtils.readFile(args[1]);
         String answerFile = args[2];
